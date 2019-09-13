ac78867b91be5812f09f1d9b1b16f8a597a90f3a
Carleton Cabarrus <carleton.cabarrus@onshoreoutsourcing.com>
2019-09-13 14:23:46 -0500

    Module 4

---
+++ StarChart/Models/CelestialObjects.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObjects
+    {
+        public int Id;
+        [Required]
+        public string Name;
+        public int? OrbitedObjectId;
+        [NotMapped]
+        public List<CelestialObjects> Satellites;
+
+        public TimeSpan OrbitalPeriod;
+    }
+}
